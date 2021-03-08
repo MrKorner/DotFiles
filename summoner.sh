44b0bd5e6ffada4d1ddfa065546c6081f5d146e8
Korner <kornersilent@gmail.com>
2021-03-08 22:26:42 +0100

    new file:   configs/kitty/kitty.conf 	modified:   summoner.sh

--- summoner.sh
+++ summoner.sh
@@ -2,4 +2,5 @@ rm -rf ./configs/* &&
 cp -r ~/.config/waybar/ ./configs/ &&
 cp ~/.config/wayfire.ini ./configs/ &&
 rsync -av --progress /etc/portage/ ./configs/portage --exclude env/ --exclude make.profile/ --exclude savedconfig/ &&
-cp /var/lib/portage/world ./configs/portage
+cp /var/lib/portage/world ./configs/portage &&
+cp -r ~/.config/kitty ./configs/
