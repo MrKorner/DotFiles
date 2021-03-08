63834610e56180c36540750603064a7fa9c27ed3
Korner <kornersilent@gmail.com>
2021-03-08 22:57:53 +0100

    hehe

--- summoner.sh
+++ summoner.sh
@@ -1,6 +1,9 @@
 rm -rf ./configs/* &&
 cp -r ~/.config/waybar/ ./configs/ &&
 cp ~/.config/wayfire.ini ./configs/ &&
-rsync -av --progress /etc/portage/ ./configs/portage --exclude env/ --exclude make.profile/ --exclude savedconfig/ &&
+rsync -av --progress /etc/portage/ ./configs/portage --exclude make.profile/ &&
 cp /var/lib/portage/world ./configs/portage &&
-cp -r ~/.config/kitty ./configs/
+cp -r ~/.config/kitty ./configs/ &&
+cp -r ~/.themes ./configs/ &&
+cp -r ~/.icons ./configs/
+cp -r ~/.config/gtk-3.0/ ./configs
