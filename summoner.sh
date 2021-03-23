21206aecaa2041a0fc498498a2d6e9a0a46c2065
MrKorner <korner@nou.com>
2021-03-23 11:15:52 +0100

    huehuehue

--- summoner.sh
+++ summoner.sh
@@ -1,13 +1,13 @@
-#rm -rf ./configs/* &&
+rm -rf ./configs/* &&
 cp -r ~/.config/waybar/ ./configs/ &&
 cp ~/.config/wayfire.ini ./configs/ &&
-#rsync -av --progress /etc/portage/ ./configs/portage --exclude make.profile/ &&
-#cp /var/lib/portage/world ./configs/portage &&
+rsync -av --progress /etc/portage/ ./configs/portage --exclude make.profile/ &&
+cp /var/lib/portage/world ./configs/portage &&
 cp -r ~/.config/kitty ./configs/ &&
 cp -r ~/.config/gtk-3.0/ ./configs &&
 cp -r ~/.bashrc ./configs/ &&
 cp -r ~/.config/wofi/ ./configs/ &&
-#cp /etc/nanorc ./configs/ &&
+cp /etc/nanorc ./configs/ &&
 cp /etc/samba/smb.conf ./configs/ &&
-#cp -r ~/.config/mc ./configs/ &&
+cp -r ~/.config/mc ./configs/ &&
 echo ":)"
